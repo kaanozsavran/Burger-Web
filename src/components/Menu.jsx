7a51ca1a83eacfa2b1989ed2bffaad1e1f24c183
Kaan Özsavran <kaanozsavran@hotmail.com>
2025-08-29 01:00:28 +0300

    updated menu page

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,5 +1,25 @@
 import React from "react";
+import MenuItem from "../components/MenuItem";
+import { Data } from "../helpers/Data";
+import "../styles/Menu.css";
 
 export const Menu = () => {
-  return <div>Menu</div>;
+  return (
+    <div className="menu">
+      <h1 className="menuTitle">Hamburgerlerimiz</h1>
+      <div className="menuList">
+        {Data.map((menuItem, key) => {
+          return (
+            <MenuItem
+              key={key}
+              image={menuItem.image}
+              name={menuItem.name}
+              content={menuItem.content}
+              price={menuItem.price}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 };
---
+++ src/helpers/Data.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import BurgerImage from "../assets/burger.jpg";
+
+export const Data = [
+  {
+    name: "Classic Cheeseburger",
+    image: BurgerImage,
+    content: "Klasik lezzet, peynirli ve nefis.",
+    price: 180,
+  },
+  {
+    name: "Bacon Burger",
+    image: BurgerImage,
+    content: "Doyurucu ve çıtır bacon ile lezzetli.",
+    price: 220,
+  },
+  {
+    name: "Double Beef Burger",
+    image: BurgerImage,
+    content: "İki kat et, ekstra doyurucu.",
+    price: 270,
+  },
+  {
+    name: "Chicken Burger",
+    image: BurgerImage,
+    content: "Tavuk etiyle hafif ve lezzetli.",
+    price: 190,
+  },
+  {
+    name: "Veggie Burger",
+    image: BurgerImage,
+    content: "Sebzelerle sağlıklı bir seçenek.",
+    price: 170,
+  },
+  {
+    name: "Spicy Burger",
+    image: BurgerImage,
+    content: "Baharat sevenler için acılı lezzet.",
+    price: 200,
+  },
+  {
+    name: "Mushroom Burger",
+    image: BurgerImage,
+    content: "Mantarlı ve kremalı nefis tat.",
+    price: 210,
+  },
+  {
+    name: "BBQ Burger",
+    image: BurgerImage,
+    content: "Barbekü sosuyla eşsiz tat.",
+    price: 230,
+  },
+  {
+    name: "Cheddar Burger",
+    image: BurgerImage,
+    content: "Erimiş cheddar peyniriyle lezzetli.",
+    price: 205,
+  },
+  {
+    name: "Hawaiian Burger",
+    image: BurgerImage,
+    content: "Ananas ve tatlı-ekşi soslu özel burger.",
+    price: 225,
+  },
+  {
+    name: "Egg Burger",
+    image: BurgerImage,
+    content: "Yumurta ile ekstra doyurucu seçenek.",
+    price: 195,
+  },
+  {
+    name: "Blue Cheese Burger",
+    image: BurgerImage,
+    content: "Mavi peynir sevenler için enfes tat.",
+    price: 240,
+  },
+];
