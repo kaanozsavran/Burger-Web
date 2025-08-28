7b776018bf51b1e0c87787d1214654c01200270e
Kaan Özsavran <kaanozsavran@hotmail.com>
2025-08-29 00:12:36 +0300

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,19 @@ import { Menu } from "./components/Menu";
 import { Contact } from "./components/Contact";
 import { About } from "./components/About";
 import { Navbar } from "./components/Navbar";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <Navbar></Navbar>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/menu" element={<Menu />}></Route>
         <Route path="/about" element={<About />}></Route>
         <Route path="/contact" element={<Contact />}></Route>
       </Routes>
+      <Footer />
     </div>
   );
 }
